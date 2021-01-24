69b2f28b60b316979aa586bc21cdf594daf67b9b
Xrenore3 <romanchaus519@gmail.com>
2021-01-24 14:48:23 +0200

    deploy with new project cart

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -4,10 +4,10 @@ const ProjectCard = ({ id, title, url }) => {
 
 
   return (
-    <a href={url} className="cardWrapper" target='_blank'>
+    <a href={url} className="cardWrapper" target='_blank' rel="noreferrer">
       <div className='card'>
-        <img src= {`./images/${title}.png`} alt={title} />
-        <h3>{title.replace('_',' ')}</h3>
+        <img src={process.env.PUBLIC_URL + `/images/${title}.png`} alt={title} />
+        <h3>{title.replace('_', ' ')}</h3>
       </div>
     </a>
   );
